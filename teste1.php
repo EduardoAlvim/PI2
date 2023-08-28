b8e1f7fedd15a91b40a6f182be1d4d406b62453f
EduardoAlvim <105020093+EduardoAlvim@users.noreply.github.com>
2023-08-28 16:26:35 -0300

    Add files via upload

---
+++ teste1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <?php
+        $t1= "Aula1";
+        $t2= "Texto1";
+        $t3= "Texto2";
+        $t4= "Texto3";
+        ?>
+        <title><?php echo $t1; ?></title>
+    </head>
+    <body>
+        <h1> <?php echo $t2; ?> </h1>
+        <h2> <?php echo $t3; ?></h2>
+        <h3> <?php echo $t4; ?></h3>
+</body>
+</html>
